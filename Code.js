74639c846a32a5421cf5072736147c1a767feb7f
Johanna Maier <joh.maier1@googlemail.com>
2022-11-19 17:46:03 +0100

    First connection of Apps Script output to Google Sheets.

--- Code.js
+++ Code.js
@@ -1,14 +1,16 @@
-// Data for SEO username & password
+// Data for SEO username, password & endpoint
 // Note: Pixel rank continues on SERP 2
 
+const username = "johanna.maier@deptagency.com";
+const pw = "c00526b67e539b1d";
 const endpoint =
   "https://api.dataforseo.com/v3/serp/google/organic/live/advanced";
 
-// user input
-const keywordIn = "Drabiny przystawne";
-const keyword = keywordIn.toLocaleLowerCase();
-const keywordClean = keyword.replace(/\s/g, "-"); // replace spaces - all of them > globally
-const domainIn = "www.canyon.com";
+
+// user input from settings tab
+const keywordIn = SpreadsheetApp.getActive().getSheetByName('Settings').getRange("A7").getValue();
+const keywordClean = keywordIn.toLocaleLowerCase().trim().replace(/ +(?= )/g," ");
+const domainIn = SpreadsheetApp.getActive().getSheetByName('Settings').getRange("B4").getValue();
 
 // HTTP request headers
 const headers = {
@@ -17,19 +19,33 @@ const headers = {
 };
 
 // HTTP request options
+// https://docs_v3.dataforseo.com/v3/keywords-data-se-locations/
+// https://docs_v3.dataforseo.com/v3/keywords-data-se-languages/
+
 const options = {
   method: "POST",
   headers: headers,
   payload: JSON.stringify([
     {
-      keyword: keyword,
+      keyword: keywordClean,
       calculate_rectangles: true,
-      language_code: "pl",
-      location_code: 2616,
+      language_code: SpreadsheetApp.getActive().getSheetByName('Settings').getRange("C3").getValue(),
+      location_code: SpreadsheetApp.getActive().getSheetByName('Settings').getRange("C2").getValue(),
     },
   ]),
   muteHttpExceptions: true,
 };
+
+// Function to write output data to sheet
+function writeDataToSheet(outputData) {
+  // If it doesn't exist yet, add a sheet where rankings are placed.
+  let rankingSheet = SpreadsheetApp.getActive().getSheetByName('SERP Rankings');
+  if(!rankingSheet) {
+    rankingSheet = SpreadsheetApp.getActive().insertSheet("SERP Rankings");
+    // rankingSheet.appendRow(["Keyword","Position","URL","Title","Meta Description"]);
+  }
+  SpreadsheetApp.getActive().getSheetByName('SERP Rankings').getRange("A1").setValue(outputData);
+}
 
 // Now we define an asynch function that waits for information of API.
 
@@ -38,37 +54,41 @@ async function httpRequest() {
   try {
     // We create an URL fetch request to endpoint with options sent in body of request.
     const response = await UrlFetchApp.fetch(endpoint, options);
-
     const data = JSON.parse(response);
-
     const allSerpResults = data["tasks"][0]["result"][0]["items"];
+    writeDataToSheet(allSerpResults);
 
-    // console.log(typeof allSerpResults);
 
-    const allOrganicResults = allSerpResults.filter((element) => {
-      return element["type"] === "organic";
-    });
+    // const allOrganicResults = allSerpResults.filter((element) => {
+    //   return element["type"] === "organic";
+    // });
 
-    const pixelsTopTen = allOrganicResults.slice(0, 10).map((element) => {
-      const pixelRank = element["rectangle"]["y"];
-      return pixelRank;
-    });
 
-    const totalPixels = pixelsTopTen.reduce((previousValue, currentValue) => {
-      return previousValue + currentValue;
-    }, 0);
-    console.log(allOrganicResults);
-    console.log(pixelsTopTen);
-    console.log(totalPixels);
+    // const pixelsTopTen = allOrganicResults.slice(0, 10).map((element) => {
+    //   const pixelRank = element["rectangle"]["y"];
+    //   return pixelRank;
+    // });
+
+    // const totalPixels = pixelsTopTen.reduce((previousValue, currentValue) => {
+    //   return previousValue + currentValue;
+    // }, 0);
+    // console.log(allOrganicResults);
+    // console.log(pixelsTopTen);
+    // console.log(totalPixels);
 
     // fs.writeFileSync(
     //   `./output/${keywordClean}.json`,
     //   JSON.stringify(rankingDomainIn)
     // );
+
+
+
   } catch (error) {
     console.log(error);
   }
 }
+
+
 
 // TO-DOs with data: Fetch current ranking position and pixel if property ranks in top 10 - maybe 100. (Done)
 // Learn how to deploy node.js with webpack to Apps Script: https://medium.com/geekculture/the-ultimate-guide-to-npm-modules-in-google-apps-script-a84545c3f57c
